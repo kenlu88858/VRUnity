0d418eb8a4945a59b0ddb7e748294863dd89c1b4
ghliu123 <zxc1030123@gmail.com>
2025-01-12 16:12:16 +0800

    選項補救成功

---
+++ Assets/select2.1.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MultipleChoiceManager1 : MonoBehaviour
+{
+    public Button[] optionButtons;
+    public Button confirmButton;
+    public static bool[] selectedOptions = new bool[6];
+    public static int currentIndex = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < optionButtons.Length; i++)
+        {
+            int index = i;
+            optionButtons[i].onClick.AddListener(() => OnOptionSelected(index));
+        }
+
+        confirmButton.onClick.AddListener(OnConfirm);
+    }
+
+    void OnOptionSelected(int index)
+    {
+        selectedOptions[index] = !selectedOptions[index];
+        Debug.Log("Option " + (index + 1) + " selected: " + selectedOptions[index]);
+    }
+
+    void OnConfirm()
+    {
+        currentIndex = 0;
+        Debug.Log("Confirm pressed, loading next selected scene.");
+        LoadNextSelectedScene();
+    }
+
+    public void LoadNextSelectedScene()
+    {
+        while (currentIndex < selectedOptions.Length)
+        {
+            if (selectedOptions[currentIndex])
+            {
+                string sceneName = "case1-" + (currentIndex + 1);
+                Debug.Log("Loading scene: " + sceneName);
+                SceneManager.LoadScene(sceneName);
+                return;
+            }
+            currentIndex++;
+        }
+
+        Debug.Log("All selected scenes complete, loading again.");
+        ResetSelections();
+        SceneManager.LoadScene("again");
+    }
+
+    public void OnNextButtonPressed()
+    {
+        currentIndex++;
+        LoadNextSelectedScene();
+    }
+
+    void ResetSelections()
+    {
+        for (int i = 0; i < selectedOptions.Length; i++)
+        {
+            selectedOptions[i] = false;
+        }
+        currentIndex = 0;
+    }
+}
+
