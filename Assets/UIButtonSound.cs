b98a61c8026e85aba2a147c2c893125c07d0764f
shark6368 <willy97011@gmail.com>
2025-03-31 22:21:06 +0800

    調整人物動畫

--- Assets/UIButtonSound.cs
+++ Assets/UIButtonSound.cs
@@ -1,53 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class UIButtonSound : MonoBehaviour, IPointerEnterHandler
 {
+    public AudioClip ClickedSound;
+    public AudioClip HoverSound;
 
-public AudioClip ClickedSound;
-public AudioClip HoverSound;
+    private Button button;
+    private AudioSource source;
 
-//get button component
-private Button button { get { return GetComponent<Button>(); } }
-// get audiosource
-private AudioSource source { get { return GetComponent<AudioSource>(); } }
-
-
-void Start()
-
-{
-    //bind an AudioSource on its
-    gameObject.AddComponent<AudioSource>();
-    //set default sound
-    source.clip = HoverSound;
-
-    source.playOnAwake = false;
-
-
-    button.onClick.AddListener(() => PlayClickSoud());
-
-
-}
-
-public void OnPointerEnter(PointerEventData eventData)
-{
-    if (eventData == null)
+    void Start()
     {
-        throw new System.ArgumentNullException(nameof(eventData));
+        // 確保這個物件上有 Button 組件
+        button = GetComponent<Button>();
+        if (button == null)
+        {
+            Debug.LogError("Button component not found on " + gameObject.name);
+            return;
+        }
+
+        // 新增 AudioSource 並綁定
+        source = gameObject.AddComponent<AudioSource>();
+        source.playOnAwake = false;
+        source.spatialBlend = 0f; // 設定為 2D 音效
+
+        // 綁定按鈕點擊事件
+        button.onClick.AddListener(PlayClickSound);
     }
 
-    source.clip = HoverSound;
-    source.PlayOneShot(HoverSound);
-}
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (HoverSound != null)
+        {
+            source.PlayOneShot(HoverSound);
+        }
+    }
 
-void PlayClickSoud()
-
-{
-
-    source.clip = ClickedSound;
-    source.PlayOneShot(ClickedSound);
-}
+    void PlayClickSound()
+    {
+        if (ClickedSound != null)
+        {
+            source.PlayOneShot(ClickedSound);
+        }
+    }
 }
