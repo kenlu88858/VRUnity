fc7727371cce1a57928605ccd7b17d8ac1d3ac6e
tommy68468 <tommychen910326@gmail.com>
2025-06-16 00:33:46 +0800

    情境前導影片字卡

---
+++ Assets/case4_ShowCanvasAfterVideo.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class case4_ShowCanvasAfterVideo : MonoBehaviour
+{
+    [Header("影片播放器")]
+    public VideoPlayer videoPlayer;
+
+    [Header("要顯示的 Canvas 群")]
+    public GameObject[] canvasGroup;
+
+    void Start()
+    {
+        // 一開始隱藏所有 Canvas
+        foreach (var canvas in canvasGroup)
+        {
+            canvas.SetActive(false);
+        }
+
+        // 當影片播完觸發事件
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        foreach (var canvas in canvasGroup)
+        {
+            canvas.SetActive(true);
+        }
+    }
+}
