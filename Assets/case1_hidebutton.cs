41cb85514cf1f01431ca4d885b1c9addec20b185
minchia00310 <jocelyn960149@gmail.com>
2025-06-21 22:02:52 +0800

    新增前導影片提示字

--- Assets/case1_hidebutton.cs
+++ Assets/case1_hidebutton.cs
@@ -14,6 +14,8 @@ public class case1_hidebutton : MonoBehaviour
     public GameObject rightButton6;
     public GameObject confirmButton;
     public AudioSource audioSource;
+    public GameObject hint_plane;
+    public GameObject hint_select;
 
     void Start()
     {
@@ -25,6 +27,8 @@ public class case1_hidebutton : MonoBehaviour
         rightButton5.SetActive(false);
         rightButton6.SetActive(false);
         confirmButton.SetActive(false);
+        hint_plane.SetActive(false);
+        hint_select.SetActive(false);
 
         // 設定影片播放完畢後要執行的函數
         videoPlayer.loopPointReached += OnVideoFinished;
@@ -32,14 +36,30 @@ public class case1_hidebutton : MonoBehaviour
 
     void OnVideoFinished(VideoPlayer vp)
     {
-        // 顯示右邊的按鈕
+        hint_plane.SetActive(true);
+        Debug.Log("plane activate");
         audioSource.Play();
+
+        // 等音訊播完再顯示按鈕
+        StartCoroutine(WaitForAudioFinish());
+        //confirmButton.SetActive(true);
+    }
+
+    private IEnumerator WaitForAudioFinish()
+    {
+    // 等待直到音訊播放完畢
+        while (audioSource.isPlaying)
+        {
+            yield return null; // 每幀檢查
+        }
+
+        // 撥放完成後再顯示按鈕
+        hint_select.SetActive(true);
         rightButton1.SetActive(true);
         rightButton2.SetActive(true);
         rightButton3.SetActive(true);
         rightButton4.SetActive(true);
         rightButton5.SetActive(true);
         rightButton6.SetActive(true);
-        //confirmButton.SetActive(true);
     }
 }
