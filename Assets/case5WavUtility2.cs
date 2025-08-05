22ea1b6fa47aac2df9a6e87a6bd25215e534f453
harry_style <106798809+harryhuang910522@users.noreply.github.com>
2025-08-05 15:22:39 +0800

    case5重複參數問題

--- Assets/case5WavUtility2.cs
+++ Assets/case5WavUtility2.cs
@@ -3,7 +3,7 @@ using System;
 using System.IO;
 using UnityEngine;
 
-public static class WavUtility2
+public static class WavUtility2_5
 {
     public static byte[] FromAudioClip(AudioClip clip)
     {
